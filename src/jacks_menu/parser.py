546153a4e201ea36634c2c14569b80b26a4310b2
EdmundGoodman <egoodman3141@gmail.com>
2025-03-28 22:59:42 +0000

    Fix parsing logic

--- src/jacks_menu/parser.py
+++ src/jacks_menu/parser.py
@@ -32,27 +32,40 @@ def neaten_date(date: str) -> str:
     return ", ".join([s.strip() for s in date.split(", ")])
 
 
-def parse_menu(location: str, web: str, menu_text: str) -> Menu:
+def parse_menu(  # noqa: C901
+    location: str, web: str, menu_text: str, debug: bool = False
+) -> Menu:
     """Parse the menu data.
 
     Args:
         location: The location of the menu.
         web: The web address of the menu.
         menu_text: The menu text to parse.
+        debug: Whether to show debugging information about the parsing state.
 
     Returns:
         The parsed menu.
     """
     lines = [line.strip() for line in menu_text.splitlines()]
 
+    non_empty_lines: int = 0
     date: str | None = None
     items: list[str] = []
     menu_parse_state = MenuParseState.Date
 
-    for line in lines:
+    for i, line in enumerate(lines):
+        if line.strip() != "":
+            non_empty_lines += 1
+
+        if debug:
+            print(
+                f"{i+1 :<2} | {non_empty_lines :<2} "
+                f"| {menu_parse_state.name :<5} | {line}"
+            )
+
         if menu_parse_state == MenuParseState.Date:
             # Last non-empty line before dash is the date
-            if line in ("-", "–"):  # noqa: RUF001
+            if line in ("-", "–") or non_empty_lines > 2:  # noqa: RUF001, PLR2004
                 menu_parse_state = MenuParseState.Items
             elif line:
                 date = neaten_date(line)
